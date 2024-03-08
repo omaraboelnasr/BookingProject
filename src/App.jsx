b4e7237578dad702069ed1fc3265a3b7116346df
Mostafa_Ali <mostafaqaraman@outlook.com>
2024-03-08 23:58:55 +0200

    deleting the unneeded files and pages, added the log in functionality

--- src/App.jsx
+++ src/App.jsx
@@ -10,35 +10,39 @@ import List from "./pages/list/List";
 import AttractionPage3 from "./pages/AttractionPage3/AttractionPage3";
 import FlightsSearch from "./pages/FlightSearch";
 import Flights from "./pages/Flights";
-import SignIn from "./pages/SignIn/SignIn";
+
 import Register from "./pages/Register/Register";
-import CreatePass from "./components/CreatePass/CreatePass";
-import TypePass from "./components/typePass/TypePass";
+import SignIn from "./pages/SignIn/SignIn";
 
 const routes = createBrowserRouter([
-  {
-    path: "/",
-    element: <AppLayOut />,
-    children: [
-      { index: true, element: <Home /> },
-      { path: "/hotels", element: <Hotels /> },
-      { path: "/hotels/rooms", element: <Rooms /> },
-      { path: "/searchflights", element: <FlightsSearch /> },
-      { path: "/flights", element: <Flights /> },
-      { path: "/attractions", element: <Attractions /> },
-      { path: "/attractions/list", element: <List /> },
-      { path: "/attractions/AttractionPage3", element: <AttractionPage3 /> },
-      { path: "/register", element: <Register /> },
-      { path: "/login", element: <SignIn /> },
-      { path: "create", element: <CreatePass /> },
-      { path: "type", element: <TypePass /> },
-      { path: "*", element: <NotFound /> },
-    ],
-  },
+	{
+		path: "/",
+		element: <AppLayOut />,
+		children: [
+			{ index: true, element: <Home /> },
+			{ path: "/hotels", element: <Hotels /> },
+			{ path: "/hotels/rooms", element: <Rooms /> },
+			{ path: "/searchflights", element: <FlightsSearch /> },
+			{ path: "/flights", element: <Flights /> },
+			{ path: "/attractions", element: <Attractions /> },
+			{ path: "/attractions/list", element: <List /> },
+			{
+				path: "/attractions/AttractionPage3",
+				element: <AttractionPage3 />,
+			},
+			{ path: "/register", element: <Register /> },
+			{ path: "/login", element: <SignIn /> },
+			{ path: "*", element: <NotFound /> },
+		],
+	},
 ]);
 
 function App() {
-  return <RouterProvider router={routes} />;
+	return (
+		<>
+			<RouterProvider router={routes} />
+		</>
+	);
 }
 
 export default App;
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,20 +1,86 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import { NavLink } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
-    return <>
-    <Navbar className ="bg-blue-900" data-bs-theme="dark" >
-    <Container style={{ maxWidth: '1100px' }}>
-            <Navbar.Brand href="/" style={{ fontWeight: 'bold' , fontSize:'25px' }}>Booking.com</Navbar.Brand>
-            <Nav className="ms-auto ">
-            <button className="ml-5 py-2 px-2 bg-white text-blue-800 rounded-sm"><NavLink to="/register" className={'text-decoration-none'}>Register</NavLink></button>
-            <button className="ml-5 py-2 px-2 bg-white text-blue-800 rounded-sm"><NavLink to="/login" className={'text-decoration-none'}>Sign in</NavLink></button>
-            </Nav>
-    </Container>
-    </Navbar>
-    </>
-}
+	const [token, setToken] = useState(localStorage.getItem("token"));
+	const logOut = () => {
+		localStorage.removeItem("token");
+		setToken(null);
+		window.location.reload();
+	};
+	useEffect(() => {
+		const handleStorageChange = () => {
+			const storedToken = localStorage.getItem("token");
+			setToken(storedToken);
+		};
+
+		window.addEventListener("storage", handleStorageChange);
+
+		return () => {
+			window.removeEventListener("storage", handleStorageChange);
+		};
+	}, [token]);
+	return (
+		<>
+			<Navbar className="bg-blue-900" data-bs-theme="dark">
+				<Container style={{ maxWidth: "1100px" }}>
+					<Navbar.Brand
+						href="/"
+						style={{ fontWeight: "bold", fontSize: "25px" }}
+					>
+						Booking.com
+					</Navbar.Brand>
+					{token ? (
+						<>
+							<div className="flex flex-col items-center pt-5">
+								<div className="flex flex-row items-center space-x-2">
+									<img
+										className="w-12 h-12 rounded-full shadow-lg"
+										src="https://placehold.co/12x12"
+										alt="Bonnie image"
+									/>
+									<p className="my-0 text-xl font-medium text-gray-100 dark:text-white">
+										Bonnie Green
+									</p>
+								</div>
+								<div className="flex mt-4 md:mt-6">
+									<a
+										onClick={logOut}
+										href="#"
+										className="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+									>
+										Logout
+									</a>
+								</div>
+							</div>
+						</>
+					) : (
+						<Nav className="ms-auto ">
+							<button className="ml-5 py-2 px-2 bg-white text-blue-800 rounded-sm">
+								<NavLink
+									to="/register"
+									className={"text-decoration-none"}
+								>
+									Register
+								</NavLink>
+							</button>
+							<button className="ml-5 py-2 px-2 bg-white text-blue-800 rounded-sm">
+								<NavLink
+									to="/login"
+									className={"text-decoration-none"}
+								>
+									Sign in
+								</NavLink>
+							</button>
+						</Nav>
+					)}
+				</Container>
+			</Navbar>
+		</>
+	);
+};
 
 export default Header;
