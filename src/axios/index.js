9b38d249e1c648f21aabf9c6d5aab6972b0b5160
OmarAboElnasr <omaraboelnasr22@gmail.com>
2024-03-09 19:06:59 +0200

    edit email validation

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/user",
+  baseURL: "http://localhost:3090/user",
   headers: {
     "Content-Type": "application/json",
   },
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,5 @@
 
 import axiosInstance from './../axiosConfig/instance';
-
 export function Regist(email,password){
  return axiosInstance.post('/user/',{email,password})
 }
