c1a8bf89472ba5f2a7f72e9d897b8dc1f30e767e
Mostafa_Ali <mostafaqaraman@outlook.com>
2024-04-16 23:15:53 +0200

    Modified header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -75,6 +75,9 @@ const Header = () => {
 			? setUserName(localStorage.getItem("userName"))
 			: setUserName("");
 	});
+	const listProperty = () => {
+		navigate("/addproperty");
+	};
 
 	return (
 		<>
@@ -87,97 +90,104 @@ const Header = () => {
 						Booking.com
 					</Navbar.Brand>
 
-					<div className="flex justify-center align-middle gap-2">
-						<div className="bg-blue-900">
-							<DropdownButton
-								title={lngs[i18n.language].nativeName}
-								id="dropdown-basic"
-								className="bg-blue-900"
-							>
-								{Object.keys(lngs).map((lng) => (
-									<Dropdown.Item
-										key={lng}
-										onClick={() => i18n.changeLanguage(lng)}
-										disabled={i18n.resolvedLanguage === lng}
-										className="flex-1 justify-center bg-blue-900 hidden"
-									>
-										{lngs[lng].nativeName}
-									</Dropdown.Item>
-								))}
-							</DropdownButton>
-						</div>
-
-						{isAuth ? (
-							<Menu
-								as="div"
-								className="relative inline-block text-left "
-							>
-								<div>
-									<Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-										{userName}
-										<ChevronDownIcon
-											className="-mr-1 h-5 w-5 text-gray-400"
-											aria-hidden="true"
-										/>
-									</Menu.Button>
-								</div>
-								<Transition
-									as={Fragment}
-									enter="transition ease-out duration-100"
-									enterFrom="transform opacity-0 scale-95"
-									enterTo="transform opacity-100 scale-100"
-									leave="transition ease-in duration-75"
-									leaveFrom="transform opacity-100 scale-100"
-									leaveTo="transform opacity-0 scale-95"
+					<div className="flex justify-center items-center gap-3">
+						<DropdownButton
+							title={lngs[i18n.language].nativeName}
+							id="dropdown-basic"
+							className="bg-blue-900 flex"
+						>
+							{Object.keys(lngs).map((lng) => (
+								<Dropdown.Item
+									key={lng}
+									onClick={() => i18n.changeLanguage(lng)}
+									disabled={i18n.resolvedLanguage === lng}
+									className="flex-1 justify-center bg-blue-900 hidden"
 								>
-									<Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-										<div className="py-1">
-											<Menu.Item>
-												<button
-													className={classNames(
-														"text-gray-700 block w-full px-4 py-2 text-left text-sm"
-													)}
-													onClick={manageAccount}
-												>
-													Manage account
-												</button>
-											</Menu.Item>
-											<Menu.Item>
-												<button
-													className={classNames(
-														"text-gray-700 block w-full px-4 py-2 text-left text-sm"
-													)}
-													onClick={logOut}
-												>
-													{t("logout")}
-												</button>
-											</Menu.Item>
-										</div>
-									</Menu.Items>
-								</Transition>
-							</Menu>
-						) : (
-							<div>
-								<Nav className="ml-9">
-									<button className="px-2 py-2 ml-5 text-blue-800 bg-white rounded-sm">
-										<NavLink
-											to="/register"
-											className={"text-decoration-none"}
-										>
-											{t("register")}
-										</NavLink>
-									</button>
-									<button className="px-2 py-2 ml-5 text-blue-800 bg-white rounded-sm">
-										<NavLink
-											to="/login"
-											className={"text-decoration-none"}
-										>
-											{t("login")}
-										</NavLink>
-									</button>
-								</Nav>
-							</div>
-						)}
+									{lngs[lng].nativeName}
+								</Dropdown.Item>
+							))}
+						</DropdownButton>
+
+						<button
+							onClick={() => listProperty()}
+							className="hover:bg-blue-500 text-white font-semibold hover:text-white py-2 px-4 hover:border-transparent rounded"
+						>
+							{t("listProperty")}
+						</button>
+						<div>
+							{isAuth ? (
+								<Menu as="div" className="relative ">
+									<div>
+										<Menu.Button className="inline-flex w-full justify-center align-middle gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+											{userName}
+											<ChevronDownIcon
+												className="-mr-1 h-5 w-5 text-gray-400"
+												aria-hidden="true"
+											/>
+										</Menu.Button>
+									</div>
+									<Transition
+										as={Fragment}
+										enter="transition ease-out duration-100"
+										enterFrom="transform opacity-0 scale-95"
+										enterTo="transform opacity-100 scale-100"
+										leave="transition ease-in duration-75"
+										leaveFrom="transform opacity-100 scale-100"
+										leaveTo="transform opacity-0 scale-95"
+									>
+										<Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+											<div className="py-1">
+												<Menu.Item>
+													<button
+														className={classNames(
+															"text-gray-700 block w-full px-4 py-2 text-left text-sm"
+														)}
+														onClick={manageAccount}
+													>
+														{t("manageAccount")}
+													</button>
+												</Menu.Item>
+												<Menu.Item>
+													<button
+														className={classNames(
+															"text-gray-700 block w-full px-4 py-2 text-left text-sm"
+														)}
+														onClick={logOut}
+													>
+														{t("logout")}
+													</button>
+												</Menu.Item>
+											</div>
+										</Menu.Items>
+									</Transition>
+								</Menu>
+							) : (
+								<div>
+									<Nav className="flex gap-2">
+										<button className="px-3 py-2 text-blue-800 bg-white rounded-sm">
+											<NavLink
+												to="/register"
+												className={
+													"text-decoration-none"
+												}
+											>
+												{t("register")}
+											</NavLink>
+										</button>
+										<button className="px-3 py-2 text-blue-800 bg-white rounded-sm">
+											<NavLink
+												to="/login"
+												className={
+													"text-decoration-none"
+												}
+											>
+												{t("login")}
+											</NavLink>
+										</button>
+									</Nav>
+								</div>
+							)}
+						</div>
 					</div>
 				</Container>
 			</Navbar>
