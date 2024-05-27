2dd6e8e9f4d77354cda55a78f81561cd546161bc
omar aboelnasr <139161001+omaraboelnasr@users.noreply.github.com>
2024-05-27 20:23:16 +0300

    Edit port (#25)
    * edit port
    
    * edit port last

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: "http://localhost:3090",
+	baseURL: "https://bookingbackend-0qe9.onrender.com/",
 	headers: {
 		"Content-Type": "application/json",
 	},
